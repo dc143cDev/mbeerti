ab2ec30b2a5414a1b0ec395d2effa470a09f3058
dc143cDev <devc143c@gmail.com>
2023-04-04 22:19:31 +0900

    템플릿으로 객체지향 최적화 완료. 잘 돌아감, 추후에 질문 컨텐츠 구성하고 다 바꾸기, 세번째 질문마다 onTap 으로 컨트롤러의 퀘스쳔 리스트 관리.

--- lib/app/modules/question/views/question1_view.dart
+++ lib/app/modules/question/views/question1_view.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:mbeerti/global/main_text.dart';
-import 'package:mbeerti/global/palette.dart';
-
-import '../../../../global/question_button.dart';
+import 'package:mbeerti/global/question_template.dart';
 import '../controllers/question_controller.dart';
 
 class Question1View extends GetView<QuestionController> {
@@ -13,94 +9,17 @@ class Question1View extends GetView<QuestionController> {
   @override
   Widget build(BuildContext context) {
     Get.put(QuestionController());
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-                flex: 3,
-                child: Text(
-                  'Q1.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'NTKR',
-                    color: hopGreen,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 25,
-                  ),
-                )),
-            Expanded(
-              flex: 2,
-              child: MainText(
-                text: '오랜만에 잡힌 술 약속, 이번 약속 장소는 내가 정하기로 했다.\n어디로 갈까?',
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Expanded(
-              flex: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    //1-A.
-                    child: QuestionButton(
-                      value: 1,
-                      codeKey: controller.hover1,
-                      isSelected: controller.isSelected1A,
-                      question: '오늘은 먹고 죽는 날. 생맥주 무한리필 호프집.',
-                      onTap: () {
-                        if (controller.isSelected1B.value == true) {
-                          controller.isSelected1A.value = true;
-                          controller.isSelected1B.value = false;
-                        } else {
-                          controller.isSelected1A.value = true;
-                        }
-                        controller.storage.write('1', 'A');
-                        print('q1: ${controller.storage.read('1')}');
-                        Get.toNamed('/question2');
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                    //1-B.
-                    child: QuestionButton(
-                      value: 2,
-                      codeKey: controller.hover2,
-                      isSelected: controller.isSelected1B,
-                      question: '기분좋게 취하기 좋은 시끌벅적한 펍.',
-                      onTap: () {
-                        if (controller.isSelected1A.value == true) {
-                          controller.isSelected1B.value = true;
-                          controller.isSelected1A.value = false;
-                        } else {
-                          controller.isSelected1B.value = true;
-                        }
-                        controller.isSelected1B.value = true;
-                        controller.storage.write('1', 'B');
-                        print('q1: ${controller.storage.read('1')}');
-                        Get.toNamed('/question2');
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+    return QuestionTemplate(
+      nav: '/question2',
+      codeKey: controller.hover1,
+      codeKeyAlt: controller.hover2,
+      isSelected: controller.isSelected1A,
+      isSelectedAlt: controller.isSelected1B,
+      storageNum: '1',
+      Qn: 'Q1.',
+      mainQ: '테스트 질문입니다.',
+      Q1: '테스트 질문의 첫 응답입니다.',
+      Q2: '테스트 질문의 두번째 응답입니다.',
     );
   }
 }
--- lib/global/question_template.dart
+++ lib/global/question_template.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mbeerti/app/modules/question/controllers/question_controller.dart';
 import 'package:mbeerti/global/palette.dart';
-import 'package:mbeerti/global/question_button.dart';
 
 import 'main_text.dart';
 
 class QuestionTemplate extends GetView<QuestionController> {
   const QuestionTemplate({
     Key? key,
-    this.nav,
-    this.question,
-    this.codeKey,
-    this.isSelected,
-    this.isSelectedAlt,
-    this.storageNum,
+    this.onTap,
+    required this.nav,
+    required this.codeKey,
+    required this.codeKeyAlt,
+    required this.isSelected,
+    required this.isSelectedAlt,
+    required this.storageNum,
     required this.Qn,
     required this.mainQ,
     required this.Q1,
@@ -22,8 +22,8 @@ class QuestionTemplate extends GetView<QuestionController> {
   }) : super(key: key);
 
   final String? nav;
-  final String? question;
   final RxObjectMixin? codeKey;
+  final RxObjectMixin? codeKeyAlt;
   final RxObjectMixin? isSelected;
   final RxObjectMixin? isSelectedAlt;
   final String? storageNum;
@@ -31,6 +31,7 @@ class QuestionTemplate extends GetView<QuestionController> {
   final String Q2;
   final String mainQ;
   final String Qn;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -76,18 +77,18 @@ class QuestionTemplate extends GetView<QuestionController> {
                       ),
                       child: InkWell(
                         onTap: () {
-                          if (isSelected?.value == true) {
+                          if (isSelectedAlt?.value == true) {
                             isSelected?.value = true;
                             isSelectedAlt?.value = false;
                           } else {
                             isSelected?.value = true;
                           }
                           controller.storage.write(storageNum!, 'A');
+                          onTap;
                           Get.toNamed(nav!);
                         },
                         onHover: (h) {
                           codeKey?.value = h;
-                          // print(controller.isHover.value.toString());
                         },
                         child: Obx(
                           () => AnimatedContainer(
@@ -98,11 +99,9 @@ class QuestionTemplate extends GetView<QuestionController> {
                                 .transform,
                             duration: Duration(milliseconds: 200),
                             curve: Curves.ease,
-                            // padding: EdgeInsets.all(codeKey?.value ? 30 : 10),
                             decoration: BoxDecoration(
                                 color:
                                     isSelected?.value ? hopGreen : Colors.white,
-                                // color: codeKey?.value ? hopGreen : Colors.white,
                                 borderRadius: BorderRadius.circular(40),
                                 boxShadow: [
                                   BoxShadow(
@@ -140,25 +139,26 @@ class QuestionTemplate extends GetView<QuestionController> {
                       ),
                       child: InkWell(
                         onTap: () {
-                          if (isSelectedAlt?.value == true) {
+                          if (isSelected?.value == true) {
                             isSelectedAlt?.value = true;
                             isSelected?.value = false;
                           } else {
                             isSelectedAlt?.value = true;
                           }
                           controller.storage.write(storageNum!, 'B');
+                          onTap;
                           Get.toNamed(nav!);
                         },
                         onHover: (h) {
-                          codeKey?.value = h;
-                          // print(controller.isHover.value.toString());
+                          codeKeyAlt?.value = h;
                         },
                         child: Obx(
                           () => AnimatedContainer(
-                            width: codeKey?.value ? 530 : 460,
-                            height: codeKey?.value ? 155 : 130,
+                            width: codeKeyAlt?.value ? 530 : 460,
+                            height: codeKeyAlt?.value ? 155 : 130,
                             transform: Transform.translate(
-                                    offset: Offset(0, codeKey?.value ? -25 : 0))
+                                    offset:
+                                        Offset(0, codeKeyAlt?.value ? -25 : 0))
                                 .transform,
                             duration: Duration(milliseconds: 200),
                             curve: Curves.ease,
